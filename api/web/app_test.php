d32b3102a520081b46d5a656d86e45383d483c22
Alex-D <contact@alex-d.fr>
2016-09-07 03:57:19 +0200

    test: configure behat to run on sqlite in test env

---
+++ api/web/app_test.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+/**
+ * @var Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('test', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);

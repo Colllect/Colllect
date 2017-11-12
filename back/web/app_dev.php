8c219a9612961c53d299ac193f6bb5d2b9b3a458
Alex-D <contact@alex-d.fr>
2017-11-12 23:57:29 +0100

    chore: setup Docker for develop env

--- back/web/app_dev.php
+++ back/web/app_dev.php
@@ -9,11 +9,7 @@ use Symfony\Component\Debug\Debug;
 umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
+if (!isset($_SERVER['SERVER_NAME']) || $_SERVER['SERVER_NAME'] !== 'colllect.dev') {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
 }

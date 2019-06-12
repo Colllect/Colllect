40b1ff84d9a46392e3300fb736b72a074b2ff04d
Alex-D <contact@alex-d.fr>
2019-06-12 17:39:40 +0200

    feat(front): redirect to login page on 401 api response

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,6 +1,13 @@
+import * as HttpStatus from 'http-status-codes'
+
 import Api from '../generated/api'
 
 const api = new Api()
+api.addErrorHandler((err) => {
+  if (err.hasOwnProperty('status') && err.status === HttpStatus.UNAUTHORIZED) {
+    window.location.replace('/login')
+  }
+})
 
 export * from '../generated/api'
 export default api

2c678ab454b7cb8bffeaf8abd0a5466bb677293b
Alex-D <contact@alex-d.fr>
2019-07-17 13:23:19 +0200

    chore(front): use browserlist and always enable autoprefixer

--- front/postcss.config.js
+++ front/postcss.config.js
@@ -1,11 +1,7 @@
-const autoprefixerConfig = {
-  browsers: [ 'last 2 versions', 'ie >= 9' ]
-}
-
 module.exports = (context) => ({
   plugins: {
     'postcss-nested': {},
-    'autoprefixer': context.env === 'production' ? autoprefixerConfig : false,
+    autoprefixer: true,
     cssnano: context.env === 'production' ? {} : false
   }
 })

1d45a406159982756e5ba96d9f5537d6b81c6873
Alex-D <contact@alex-d.fr>
2019-09-26 15:49:37 +0200

    feat(front): add light button

--- front/src/components/button/Button.ts
+++ front/src/components/button/Button.ts
@@ -1,6 +1,11 @@
 import {Component, Prop, Vue} from 'vue-property-decorator'
 import WithRender from './Button.html'
 
+enum ButtonType {
+  Default = 'default',
+  Light = 'light',
+}
+
 @WithRender
 @Component
 export default class ColllectButton extends Vue {
@@ -11,6 +16,9 @@ export default class ColllectButton extends Vue {
   @Prop({default: false})
   private disabled!: boolean
 
+  @Prop({default: ButtonType.Default})
+  private type!: ButtonType
+
   /*
    * Computed
    */
@@ -18,6 +26,7 @@ export default class ColllectButton extends Vue {
   get classes() {
     return {
       'c-colllect-button__disabled': this.disabled,
+      ['c-colllect-button__type-' + this.type]: this.type !== ButtonType.Default,
     }
   }
 

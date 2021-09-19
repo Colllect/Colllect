7ce5b6f19a93ff340999efb7646a7209f99ced9e
Alex-D <contact@alex-d.fr>
2021-09-20 01:39:42 +0200

    chore(ci): add front typecheck

---
+++ back/src/Command/ColllectOpenapiDumpCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Nelmio\ApiDocBundle\ApiDocGenerator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ColllectOpenapiDumpCommand extends Command
+{
+    protected static $defaultName = 'colllect:openapi:dump';
+    protected static string $defaultDescription = 'Dump OpenAPI definition as JSON';
+
+    public function __construct(
+        private ApiDocGenerator $generator,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription(self::$defaultDescription)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln(json_encode($this->generator->generate()->toArray()));
+
+        return 0;
+    }
+}
--- front/scripts/generateApiClient.js
+++ front/scripts/generateApiClient.js
@@ -7,19 +7,34 @@ const needle = require('needle')
 
 const generatedFolderPath = path.normalize('src/generated/api')
 
-const swaggerUrl = 'https://dev.colllect.io/api/doc.json'
-
-console.info('Fetching swagger description file from', swaggerUrl)
-needle('get', swaggerUrl)
-	.then(async (response) => {
-		await OpenAPI.generate({
-			input: response.body,
-			output: generatedFolderPath,
-		})
+const generate = (input) => {
+	OpenAPI.generate({
+		input,
+		output: generatedFolderPath,
+	}).then(() => {
 		console.info(generatedFolderPath, 'was successfully updated!')
+		process.exit(0)
+	}).catch((err) => {
+		console.error('Unable to generate client. Is the definition file valid?')
+		console.error(err)
+		process.exit(1)
+	})
+}
+
+if (process.env.CI) {
+	generate(generatedFolderPath + '.json')
+	return
+}
+
+const openApiUrl = 'https://dev.colllect.io/api/doc.json'
+
+console.info('Fetching swagger definition file from', openApiUrl)
+needle('get', openApiUrl)
+	.then((response) => {
+		generate(response.body)
 	})
 	.catch((err) => {
-		console.error('Unable to fetch swagger description file. Did you run the `make up` command?')
+		console.error('Unable to fetch OpenAPI definition file. Did you run the `make up` command?')
 		console.error(err)
 		process.exit(1)
 	})
